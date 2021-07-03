5a88b88f4b3cc80c5019f4b1fd35018a9ab4eebd
masoncusack <1masoncusack@gmail.com>
2021-07-03 21:54:05 +0100

    update demo image with funnier example

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 title = st.title("arXiv abstracts simplifier 📖🤯👶")
 text_field = st.text_input(label="Enter an arXiv abstract that you don't get. Press enter to submit.")
 reading_age = st.slider(
-    label="Choose your reading age in the field of this paper. 0 is newborn, 117 is superhuman (for now).",
+    label="Choose your reading age in the field of this paper. 1 is practically newborn, 117 is superhuman (for now).",
     min_value=1,
     max_value=117
 )
 
-openai_prompt = f"An {reading_age} year old asked me what this passage means:\n\"\"\"\n{text_field}\n\"\"\"\nI rephrased it for them, in plain language a {reading_age} year old can understand:\n\"\"\"\n"
+if reading_age == 0:
+    recipient_prompt = "newborn baby"  # TODO: model does not respond well to "newborn baby" - for now I've fixed min above at 1.
+else:
+    recipient_prompt = f"{reading_age} year old"
+openai_prompt = f"A {recipient_prompt} asked me what this abstract means:\n\"\"\"\n{text_field}\n\"\"\"\nI rephrased it for them, in plain language a {recipient_prompt} can understand:\n\"\"\"\n"
 
 response = openai.Completion.create(
   engine="davinci",
